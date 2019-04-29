0f39bfd5bda7c4d2f66b3f2065d14b3a773f3128
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-04-29 18:15:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ data.then(function(data)
     
   },
   function(err){console.log(err)})
+
+var screen = {height:600, width:600}
+
+var margins = {top:10, left:10, right: 10, bottom:10}
+
+var svg = d3.select("svg")
+            .attr("width", screen.width)
+            .attr("height", screen.height)
