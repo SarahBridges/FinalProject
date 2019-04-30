ad45995b2d450a883a1600fbdd8ca25d38aefbcb
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-04-30 14:54:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ var Earnings = function(edu)
 
   var yScale = d3.scaleBand()
                  .range([0, screen.height])
-                 .domain(data.map(function(d){return d.EducationalAttainment}));
+                 .domain(dataP.map(function(d){return d.EducationalAttainment}));
   //axes
   var xAxis = svg.append("g")
                  .attr("transform", "translate(0," + screen.height + ")")
