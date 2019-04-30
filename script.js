04e42017b3bc34bb26da4caaf3e5de7d79a49b61
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-04-30 14:12:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,24 @@ var screen = {height:600, width:600}
 
 var margins = {top:10, left:10, right: 10, bottom:10}
 
-var svg = d3.select("svg")
-            .attr("width", screen.width)
-            .attr("height", screen.height)
+var xScale = d3.scaleLinear()
+               .domain([])
+               .range([0, width])
+var yScale = d3.scaleLinear()
+               .domain([])
+               .range([0, height])
+
+var Earnings = function(data)
+{
+  var svg = d3.select("svg")
+              .attr("width", screen.width)
+              .attr("height", screen.height)
+
+  svg.selectAll("rect")
+     .data(dataP)
+     .enter()
+     .append("rect")
+     .attr("x",xScale(0))
+     .attr("y", function(d){return yScale(d.MedianUsualWeeklyEarnings)})
+}
+
