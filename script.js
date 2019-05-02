28f7a06eadcd59650b35aa7be29e5ec665c2211d
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-02 14:22:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ var Earnings = function(edu)
 
   var yAxis = d3.axisLeft(yScale)
                 .ticks(9)
-                .tickFormat(function(d,i){console.log("d.EA",d.EducationalAttainment); return d.EducationalAttainment});
+                .tickFormat(function(d,i){console.log("d.EA",edu[d].EducationalAttainment); return edu[d].EducationalAttainment});
       svg.append("g").classed("yAxis", true)
                  .call(yAxis)
                  .attr("transform", "translate(" + (margins.left+100) + ", 30)");
