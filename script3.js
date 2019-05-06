199d156a6bc360c194e5343b5798c186755d83be
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-06 14:24:05 -0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -3,8 +3,8 @@ var data = d3.csv("EducationData.csv");
 data.then(function(data)
 {
   console.log("data:", data);
-  console.log("d.EducationalAttainment:", function(d){return d.EducationalAttainment});
-  console.log("d.UnemploymentRate:", function(d){return d.UnemploymentRate});
+  console.log("d.EducationalAttainment:", data.EducationalAttainment);
+  console.log("d.UnemploymentRate:", data.UnemploymentRate);
 },
 function(err){console.log(err);
 })
