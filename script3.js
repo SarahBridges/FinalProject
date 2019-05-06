726b58c75e90db540bc8f7cefb0627e5ece7e253
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-06 14:57:25 -0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -14,17 +14,40 @@ var drawChart = function(data)
      .enter()
      .append("rect")
      .attr("x", function(d,i){return i*barwidth;})
-     .attr("y", function(d){console.log("d",d); console.log("d.UR",d.UnemploymentRate); return height - d.UnemploymentRate})
+     .attr("y", function(d){console.log("d",d); 
+                            console.log("d.UR",d.UnemploymentRate); 
+                            return height - d.UnemploymentRate})
      .attr("width", barwidth)
      .attr("height", function(d){return d.UnemploymentRate*400})
      .attr("fill", "blue")
 
 }
+var drawChart2 = function(data)
+{
+  var width = 600;
+  var height = 600;
+  var barheight = height/data.length;
+  var svg = d3.select("#chart")
+              .attr("width", width)
+              .attr("height", height)
+  
+  svg.selectAll("rect")
+     .data(data)
+     .enter()
+     .append("rect")
+     .attr("x", function(d,i)
+           {console.log("d", d); 
+            console.log("d.UR", d.UnemploymentRate); 
+            return height - d.UnemploymentRate;})
+     .attr("y", function(d,i){return i*barheight;})
+     .attr("width", function(d){return d.UnemploymentRate})
+     .attr("height", barheight)
+}
 
 data.then(function(data)
 {
   console.log("data:", data);
-  drawChart(data);
+  drawChart2(data);
 },
 function(err){console.log(err);
 })
