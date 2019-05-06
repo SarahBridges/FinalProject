853105550c263d4b98f1cd4b8c1775963d964e49
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-06 01:57:05 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,11 +1,11 @@
 d3.csv("EducationData.csv", function(data){
 
-  console.log("dataP:", dataP)
+  console.log("data:", data)
   var screen = {height: 600, width:600}
   var margins = {top:10, bottom:10, left:10, right:10}
   var width = screen.width - margins.left - margins.right
   var height = screen.height - margins.top - margins.bottom
-  var barHeight=height/dataP.length;
+  var barHeight=height/data.length;
   
   var svg = d3.select("#chart")
               .attr("height", screen.height)
@@ -23,12 +23,12 @@ d3.csv("EducationData.csv", function(data){
   
   var yScale = d3.scaleBand()
                  .range([0,height])
-                .domain(dataP.map(function(d){console.log(dataP.UnemploymentRate); return d.UnemploymentRate}));
+                .domain(data.map(function(d){console.log(d.UnemploymentRate); return d.UnemploymentRate}));
   svg.append("g")
      .call(d3.axisLeft(yScale))
      //.classed("yAxis", true).call(yAxis)
 
-  svg.selectAll("rect").data(dataP).enter().append("rect")
+  svg.selectAll("rect").data(data).enter().append("rect")
      .attr("x", xScale(0))
      .attr("y", yScale(function(d){return d.EducationalAttainment}))
      .attr("width", function(d){return xScale(d.UnemploymentRate)})
