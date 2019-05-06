caa5eb5c229b88e1fcbc184b1f4854711073f903
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-06 14:39:26 -0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,10 +1,27 @@
 var data = d3.csv("EducationData.csv");
 
+var drawChart = function(data)
+{
+  var width = 600;
+  var height = 600;
+  var barwidth = width/data.length;
+  var svg = d3.select("#chart")
+              .attr("width", width)
+              .attr("height", height)
+  
+  svg.selectAll("rect")
+     .data(data)
+     .enter()
+     .append("rect")
+     .attr("x", function(d,i){return i*barwidth;})
+     .attr("y", function(d){console.log(d); return height - d.UnemploymentRate})
+
+}
+
 data.then(function(data)
 {
   console.log("data:", data);
-  console.log("d.EducationalAttainment:", data.EducationalAttainment);
-  console.log("d.UnemploymentRate:", data.UnemploymentRate);
+  drawChart(data);
 },
 function(err){console.log(err);
 })
