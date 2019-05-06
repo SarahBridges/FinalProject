809c7c68907dffb11c892872993519c02e54fdff
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-06 15:29:05 -0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -44,10 +44,30 @@ var drawChart2 = function(data)
      .attr("height", barheight)
 }
 
+var drawLabels = function(data)
+{
+  var width = 600;
+  var height = 600;
+  var barheight = height/data.length;
+  
+  var svg = d3.select("#chart")
+              .attr("width", width)
+              .attr("height", height)
+  svg.selectAll("text")
+     .data(data)
+     .enter()
+     .append("text")
+     .text(function(d){return d.UnemploymentRate})
+     .attr("x", function(d,i){return d.UnemploymentRate})
+     .attr("y", function(d,i){return i* barheight})
+     .attr("fill", "blue")
+}
+
 data.then(function(data)
 {
   console.log("data:", data);
   drawChart2(data);
+  drawLabels(data);
 },
 function(err){console.log(err);
 })
