a734116913f246076e450e66b0c611292bd25706
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-06 00:24:34 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -15,7 +15,7 @@ var drawChart=function(dataP)
   var yScale = d3.scaleLinear().domain([0,dataP.length]).range([height,0]);
   
   svg.selectAll("rect").data(dataP).enter().append("rect")
-     .attr("y", function(d){return xScale(d.EducationalAttainmentP)}).attr("x", function(d,i){return (barHeight*i)})
+     .attr("y", function(d){return yScale(d.EducationalAttainmentP)}).attr("x", function(d,i){return xScale(barHeight*i)})
   
   var xAxis = d3.axisTop(xScale).ticks(10)
   var yAxis = d3.axisLeft(yScale).ticks(dataP.lentgh)
