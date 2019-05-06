cd743d3e4d7d5ec23167adc8f3bc2cb7a422afe7
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-06 01:26:38 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -9,21 +9,41 @@ var drawChart=function(dataP)
   var height = screen.height - margins.top - margins.bottom
   var barHeight=height/dataP.length;
   
-  var svg = d3.select("#chart").attr("height", screen.height).attr("width", screen.width)
+  var svg = d3.select("#chart")
+              .attr("height", screen.height)
+              .attr("width", screen.width)
+              .attr("transform" "translate(" + margins.left + "," + margins.top + ")")
   
   var xScale = d3.scaleLinear().domain([0, 2000]).range([0, width]);
-  var yScale = d3.scaleLinear().domain([0,dataP.length]).range([height,0]);
+  svg.append("g")
+     .attr("transform", "translate(0," + height +")")
+     .call(d3.axisBottom(xScale))
+     .selectAll("text")
+     .attr("transfomr", "translate(-10,0)rotate(-45)")
+     .style("text-anchor", "end")
+//     .classed("xAxis", true).call(xAxis)
+  
+  var yScale = d3.scaleBand()
+                 .range([0,height])
+                .domain(dataP.map(fuction(d){return d.UnemploymentRate}));
+  svg.append("g")
+     .call(d3.axisLeft(yScale))
+     //.classed("yAxis", true).call(yAxis)
 
   svg.selectAll("rect").data(dataP).enter().append("rect")
-     .attr("x", function(d){return d.EducationalAttainmentP})
-     .attr("y", function(d,i){return (barHeight*i)})
+     .attr("x", xScale(0)
+     .attr("y", yScale(fuction(d){return d.UnemploymentRate})
+     .attr("width", function(d){return xScale(d.UnemploymentRate)})
+     .attr("height", function(d){return yScale.bandwidth()})
      .attr("fill", "blue")
   
   //var xAxis = d3.axisBottom(xScale).ticks(10)
   //var yAxis = d3.axisLeft(yScale).ticks(dataP.lentgh)
   
-  svg.append("g").classed("xAxis", true).call(xAxis)
-  svg.append("g").classed("yAxis", true).call(yAxis)
+           //function(d,i){return (barHeight*i)})
+           //function(d){return d.EducationalAttainmentP})
+  
+
 }
 
 dataP.then(function(dataP){drawChart(dataP)},function(err){console.log(err)})
