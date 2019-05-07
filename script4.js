89ef6f1ef7d9cbe555270200cf4bc6dbb9b0f0a4
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-06 21:48:04 -0400

    Update script4.js

--- script4.js
+++ script4.js
@@ -29,7 +29,7 @@ var drawAxisLabels = function(data)
      .append("text")
      .text(function(d){return d.EducationalAttainment})
      .attr("x", function(d){return d})
-     .attr("y", function(d,i){return i*barheight})
+     .attr("y", function(d,i){return i*5})
      .attr("transform", "translate(0, 100)")
 
 }
