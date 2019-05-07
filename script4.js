93fe14f30939e92d27e8ce58b45f9e2f35db1c1b
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-06 21:34:15 -0400

    Update script4.js

--- script4.js
+++ script4.js
@@ -16,7 +16,22 @@ function(err)
 
 var drawAxisLabels = function(data)
 {
+  var width = 200
+  var height = 600
+  var barheight = height/data.length
+  
   var svg = d3.select("#axisLabels")
+              .attr("width", width)
+              .attr("height", height)
+  
+  svg.selectAll("text")
+     .data(data)
+     .enter()
+     .append("text")
+     .text(function(d){return d.EducationalAttainment})
+     .attr("x", function(d){return d})
+     .attr("y", function(d,i){return i*barheight})
+
 }
 
 var drawAxes = function(data)
