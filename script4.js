41976cba3de1763578a41b05a77cb5b688deff47
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-07 00:22:29 -0400

    Update script4.js

--- script4.js
+++ script4.js
@@ -61,6 +61,7 @@ var update = function(data, button)
 {
   d3.select("svg").remove();
   
+   //sizes
   var width = 600
   var height = 600
   var barheight = height/data.length
@@ -68,27 +69,22 @@ var update = function(data, button)
   var svg = d3.select("body").append("svg")
               .attr("width", width+200)
               .attr("height", height+100)
-  
-  if (button=="UR"){var num = 6;}
-  else if (button=="ME"){var num = 2000;}
-  else if (button=="MP"){var num = 15;}
-  else if (button=="WP"){var num = 20;}
-  console.log("num:", num)
-    //sizes
-
   //axis labels
   svg.selectAll("text")
      .data(data)
      .enter()
      .append("text")
-     .text(function(d){return num})
+     .text(function(d){return d.EducationalAttainment})
      .attr("x", function(d){return d})
      .attr("y", function(d,i){return i*70})
      .attr("transform", "translate(0, 65)")
   //axes
     //scales
   var xScale = d3.scaleLinear()
-                 .domain([0, num])
+                 .domain([0, function(d){if(button=="UR"){return 6}
+                                         else if (button="ME"){return 2000}
+                                         else if (button="MP"){return 15}
+                                         else if (button="WP"){return 20}}])
                  .range([0, width])
 
   var yScale = d3.scaleLinear()
@@ -105,18 +101,17 @@ svg.selectAll("rect")
      .data(data)
      .enter()
      .append("rect")
-     .attr("x", function(d,i){
-          if (button=="UR"){return d.UnemploymentRate;}
-          else if (button=="ME"){return d.MedianUsualWeeklyEarnings;}
-          else if (button=="MP"){return d.MenBelowPovertyLevel;}
-          else if (button=="WP"){return d.WomenBelowPovertyLevel;};})
+     .attr("x", function(d,i){if(button=="UR"){return d.UnemploymentRate}
+                              else if (button="ME"){return d.MedianUsualWeeklyEarnings}
+                              else if (button="MP"){return d.MenBelowPovertyLevel}
+                              else if (button="WP"){return d.WomenBelowPovertyLevel}})
      .attr("y", function(d,i){return (i*barheight)})
-     .attr("width", function(d){
-          if (button=="UR"){return xScale(d.UnemploymentRate);}
-          else if (button=="ME"){return xScale(d.MedianUsualWeeklyEarnings);}
-          else if (button=="MP"){return xScale(d.MenBelowPovertyLevel);}
-          else if (button=="WP"){return xScale(d.WomenBelowPovertyLevel);};})
+     .attr("width", function(d){if(button=="UR"){return xScale(d.UnemploymentRate)}
+                                else if (button="ME"){return xScale(d.MedianUsualWeeklyEarnings)}
+                                else if (button="MP"){return xScale(d.MenBelowPovertyLevel)}
+                                else if (button="WP"){return xScale(d.WomenBelowPovertyLevel)}})
      .attr("height", barheight-5)
      .attr("fill", "teal")
      .attr("transform", "translate(181, 49)")
+
 }
