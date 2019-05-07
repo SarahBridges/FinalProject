2046bf9652c46094af15cc8c5bfcbeeed6d74a0d
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-05-06 22:19:15 -0400

    Update script4.js

--- script4.js
+++ script4.js
@@ -42,6 +42,6 @@ var initialize = function(data)
   var xAxis = d3.axisBottom().scale(xScale)
   var yAxis = d3.axisLeft().scale(yScale)
   
-  svg.append("g").classed("xAxis", true).call(xAxis).attr("transform", "translate(50, 570)")
-  svg.append("g").classed("yAxis", true).call(yAxis).attr("transform", "translate(50, 65)")
+  svg.append("g").classed("xAxis", true).call(xAxis).attr("transform", "translate(100, 550)")
+  svg.append("g").classed("yAxis", true).call(yAxis).attr("transform", "translate(100, 65)")
 }
